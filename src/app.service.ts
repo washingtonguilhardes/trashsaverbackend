930256c6be0377b1197e9da4798fff0f479eb058
washingtonguilhardes <washington.guilhardes@gmail.com>
2022-09-14 21:38:11 -0300

    fix: remove lint warning messages

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,12 +1,4 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService {
-  getLoginUrl(clientId: string) {
-    return `https://login.microsoftonline.com/${process.env.MSAL_TENANT_ID}/adminconsent?client_id=${clientId}&redirect_uri=${process.env.MSAL_OAUTH_REDIRECT_URI}&state=1`;
-  }
-
-  async setupApp(code: string) {
-    return 'DONE';
-  }
-}
+export class AppService {}
